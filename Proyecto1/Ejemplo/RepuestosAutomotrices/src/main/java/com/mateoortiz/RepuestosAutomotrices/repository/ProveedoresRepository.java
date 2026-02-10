7a38f978cd5ca78caf1a22d8ab68c177ff76b396
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-10 09:12:02 -0600

    Feat: Se agrego el ProveedoresRepository

--- Proyecto1/Ejemplo/RepuestosAutomotrices/src/main/java/com/mateoortiz/RepuestosAutomotrices/repository/ProveedoresRepository.java
+++ Proyecto1/Ejemplo/RepuestosAutomotrices/src/main/java/com/mateoortiz/RepuestosAutomotrices/repository/ProveedoresRepository.java
@@ -1,4 +1,10 @@
 package com.mateoortiz.RepuestosAutomotrices.repository;
 
-public interface ProveedoresRepository {
+import com.mateoortiz.RepuestosAutomotrices.entity.Empleado;
+import com.mateoortiz.RepuestosAutomotrices.entity.Proveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProveedoresRepository extends JpaRepository<Proveedor,Integer> {
 }
